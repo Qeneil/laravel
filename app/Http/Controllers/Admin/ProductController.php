fffb4515ff3e2133b0710dccdc9c20119474b240
Qeneil <durfkung456@gmail.com>
2024-02-22 03:26:35 +0700

    22/02

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Subcategory;
+use App\Models\Product;
+
 
 class ProductController extends Controller
 {
@@ -28,11 +30,37 @@ class ProductController extends Controller
             'product_subcatgory_id' => 'required' ,
             'product_img' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048' ,
         ]);
+ 
+        $image = $request->file('product_img');
+        $img_name = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        $request->product_img->move(public_path('upload'),$img_name);
+        $img_url ='upload/' .$img_name;
+
         $category_id = $request ->product_category_id;
         $subcategory_id =$request ->product_subcatgory_id; 
 
         $category_name =Category::where('id',$category_id)->value('category_name');
         $subcategory_name =Subcategory::where('id',$subcategory_id)->value('subcategory_name');
+
+
+        Product::insert([
+            'product_name' => $request->product_name,
+            'product_short_des' => $request->product_short_des,
+            'product_long_des' => $request->product_long_des,
+            'price' => $request->price,
+            'product_category_name' =>  $category_name,
+            'product_subcatgory_name' => $subcategory_name,
+            'product_category_id' => $request->product_category_id,
+            'product_subcatgory_id' => $request->product_subcatgory_id,
+            'product_img' => $img_url,
+            'quantity' => $request->quantity,
+            'slug' => strtolower(str_replace('','-',$request->subcategory_name)),
+
+
+        ]);
+        Category::where('id',$category_id)->increment('product_count',1);
+        Subcategory::where('id',$subcategory_id)->increment('product_count',1);
+        return redirect()->route('allproduct')->with('message' , 'add product successfully');
 
     }
 }
